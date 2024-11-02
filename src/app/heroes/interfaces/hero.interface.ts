560ae4428e81d3be3ad1ab576e8ef2f38b54912c
Jonathan <contactoanonimo.x@gmail.com>
2024-11-02 15:35:01 -0600

    http

---
+++ src/app/heroes/interfaces/hero.interface.ts
@@ -0,0 +1,25 @@
+// export interface Hero {
+//   users:  User[];
+//   heroes: HeroElement[];
+// }
+
+//export interface HeroElement {
+export interface Hero {
+  id: string;
+  superhero: string;
+  publisher: Publisher;
+  alter_ego: string;
+  first_appearance: string;
+  characters: string;
+}
+
+export enum Publisher {
+  DCComics = "DC Comics",
+  MarvelComics = "Marvel Comics",
+}
+
+// export interface User {
+//   id:      number;
+//   usuario: string;
+//   email:   string;
+// }
